627c4af64de7064a8d4237a5108fdcb0e7943a22
Ani <a.usanetashvili1@gmail.com>
2024-12-05 23:45:10 +0400

    Combined functionalities

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,26 +13,20 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.ApplicationFunctionality
 
 class MainActivity : AppCompatActivity() {
+
+
+
     @SuppressLint("WrongViewCast", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
-        //val emailField = findViewById<EditText>(R.id.email)
-        val usernameField = findViewById<EditText>(R.id.username)
-//        val firstNameField = findViewById<EditText>(R.id.first_name)
-//        val lastNameField = findViewById<EditText>(R.id.last_name)
-//        val ageField = findViewById<EditText>(R.id.age)
-//        val text1 = findViewById<TextView>(R.id.text1)
-//        val saveButton = findViewById<Button>(R.id.save_btn)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Checks empty fields and prints messages
         checkEmptyFields()
-        checkUsernameLength()
-        validEmail()
+        clearFields()
     }
-
-
 
     private fun filledFields(fieldName:EditText): String {
         //Checks whether fields are empty or not, saves messages into a list and returns messages
@@ -61,58 +55,69 @@ class MainActivity : AppCompatActivity() {
         }
 
         return if (messages.isNotEmpty()) {
-            val finalMessage = messages.joinToString("\n")
-            text1.text = finalMessage
-            finalMessage
+            text1.text = messages.joinToString("\n")
+            text1.text.toString()
         } else {
-            val successMessage = "All fields are filled."
-            text1.text = successMessage
-            successMessage
+            ""
         }
     }
 
     private fun checkEmptyFields() {
         val emailField = findViewById<EditText>(R.id.email)
-        val usernameField = findViewById<EditText>(R.id.username)
-        val firstNameField = findViewById<EditText>(R.id.first_name)
-        val lastNameField = findViewById<EditText>(R.id.last_name)
-        val ageField = findViewById<EditText>(R.id.age)
         val saveButton = findViewById<Button>(R.id.save_btn)
 
         saveButton.setOnClickListener{
+            checkUsernameLength()
+            validEmail()
             filledFields(emailField)
-            filledFields(usernameField)
-            filledFields(firstNameField)
-            filledFields(lastNameField)
-            filledFields(ageField)
         }
     }
     //Function checkes whether username has more than 10 symbols or not and returns message
     private fun checkUsernameLength(): String {
         val text2 = findViewById<TextView>(R.id.text2)
-        val saveButton = findViewById<Button>(R.id.save_btn)
         val usernameField = findViewById<EditText>(R.id.username)
 
-        saveButton.setOnClickListener{
-            text2.text = if(usernameField.text.length < 10 ) {
-                "Username Should have more than 10 symbols."
-            }else{
-                ""
-            }
+        if(usernameField.text.length < 10 ) {
+            text2.text = "Username Should have more than 10 symbols."
+        }else{
+            text2.text = ""
         }
+
         return text2.text.toString()
     }
 
 
-    private fun validEmail(): String? {
-        val emailField = findViewById<EditText>(R.id.email).toString()
-        var text3 = findViewById<TextView>(R.id.text3).toString()
+    private fun validEmail(): String {
+        val emailField = findViewById<EditText>(R.id.email)
+        val saveButton = findViewById<Button>(R.id.save_btn)
+        val text3 = findViewById<TextView>(R.id.text3)
+        val messages = mutableListOf<String>()
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(emailField).matches()){
-            text3 = "Invalid Email address."
-            return text3
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailField.text.toString()).matches()) {
+            text3.text = "Invalid Email address."
+        } else
+            text3.text = ""
+
+        return text3.text.toString()
+    }
+
+//    When "Clear" button is pressed for a long time all field inputs are deleted
+    private fun clearFields() {
+        val emailField = findViewById<EditText>(R.id.email)
+        val usernameField = findViewById<EditText>(R.id.username)
+        val firstNameField = findViewById<EditText>(R.id.first_name)
+        val lastNameField = findViewById<EditText>(R.id.last_name)
+        val ageField = findViewById<EditText>(R.id.age)
+        val clearButton = findViewById<Button>(R.id.clear_btn)
+        clearButton.setOnLongClickListener{
+            emailField.text.clear()
+            usernameField.text.clear()
+            firstNameField.text.clear()
+            lastNameField.text.clear()
+            ageField.text.clear()
+            true
         }
-        return null
     }
 }
 
