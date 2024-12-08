504a8fe0d4306da5c55f7b11c092fa7ae3ae3cc8
Ani <a.usanetashvili1@gmail.com>
2024-12-08 23:20:39 +0400

    Added field validations and function which saves user info into map

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,19 +2,67 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.Users
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast", "MissingInflatedId")
+    private var usersList = mutableMapOf<String, String>()
+    private var count = 0;
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        validateFields()
+
+    }
+
+    private fun addUser(){
+        val email = binding.email.text.toString()
+        val name = binding.fullName.text.toString()
+
+        if(usersList.containsKey(email)){
+            binding.userCount.text = "The user with this email already exists!"
+        }else{
+                usersList[email] = name
+                count += 1
+                binding.userCount.text = "User added."
+        }
+
+    }
+
+
+    private fun validateFields() {
+        binding.addBtn.setOnClickListener {
+            if(binding.fullName.text.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(binding.email.text.toString()).matches()) {
+                addUser()
+                binding.nameText.text = ""
+                binding.emailText.text = ""
+            }else if(binding.fullName.text.isNotEmpty()){
+                binding.nameText.text = ""
+            }else if(binding.fullName.text.isEmpty()){
+                binding.nameText.text = "Full name field is empty."            }
+
+            if(binding.email.text.isEmpty()) {
+                binding.emailText.text = "Email field is empty."
+            }else if(Patterns.EMAIL_ADDRESS.matcher(binding.email.text.toString()).matches() && binding.fullName.text.isEmpty()){
+                binding.emailText.text = ""
+            }
+            else if (!Patterns.EMAIL_ADDRESS.matcher(binding.email.text.toString()).matches() && binding.email.text.isNotEmpty()){
+                binding.emailText.text = "Incorrect email format."
+            }
+
+        }
 
     }
 }
+
 
---
+++ app/src/main/java/com/example/myapplication/Users.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+data class Users(val name: String, val email: String){
+
+}
