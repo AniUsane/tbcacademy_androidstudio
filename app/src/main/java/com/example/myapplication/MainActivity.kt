c0aafa9d2e90fb4093d0a7076740dbf7f5dac5f2
Ani <a.usanetashvili1@gmail.com>
2024-12-20 10:45:19 +0400

    Committed

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,48 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.View
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
+import java.security.AccessController.getContext
+import java.util.Locale
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var users: ArrayList<User>
+    private lateinit var titleList:Array<String>
+    private lateinit var searchView: SearchView
+    private lateinit var searchList: ArrayList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recycleView.layoutManager = LinearLayoutManager(this)
+        binding.recycleView.setHasFixedSize(true)
+
+
+        users = arrayListOf<User>()
+        searchList = arrayListOf<User>()
+        getContext()
+
+        searchView.clearFocus()
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchView.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchList.clear()
+
+                return false
+            }
+
+        })
 
 
 
---
+++ app/src/main/java/com/example/myapplication/User.kt
@@ -0,0 +1,5 @@
+package com.example.myapplication
+
+class User(val id:Int, val firstName:String, val lastName:String, val birthday:String, val address:String, val email:String) {
+
+}
