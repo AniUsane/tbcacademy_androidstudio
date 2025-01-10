1059537f182bf4f403f10fb4c0da155e1be818ef
Ani <a.usanetashvili1@gmail.com>
2025-01-11 03:02:17 +0400

    Added navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.utils.addToStdlib.safeAs
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.safeargs)
 }
 
 android {
@@ -48,4 +51,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
 }
