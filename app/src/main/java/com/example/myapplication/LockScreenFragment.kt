946ab70d092c1dac2342760209cc64e4ee3baee5
Ani <a.usanetashvili1@gmail.com>
2025-02-07 02:01:28 +0400

    Finished project

--- app/src/main/java/com/example/myapplication/LockScreenFragment.kt
+++ app/src/main/java/com/example/myapplication/LockScreenFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.myapplication.databinding.FragmentLockScreenBinding
@@ -9,7 +10,62 @@ class LockScreenFragment : BaseFragment<FragmentLockScreenBinding>(FragmentLockS
 
     override fun start() {
         binding.recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
-        binding.recyclerView.adapter = NumpadAdapter(viewModel.numpadButtons)
+        binding.recyclerView.adapter = NumpadAdapter(viewModel.numpadButtons, { number ->
+            enterPassword(number)}, {deleteNumber()} )
+        checkPassword()
     }
+
+    val enteredPassword = mutableListOf<Int>()
+
+    private fun enterPassword(number: Int){
+        if (enteredPassword.size < 4) {
+            enteredPassword.add(number)
+            updateDots()
+
+            if (enteredPassword.size == 4) {
+                checkPassword()
+            }
+        }
+    }
+
+    private fun checkPassword(){
+        if (enteredPassword.joinToString("") == "0934"){
+            Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+            enteredPassword.clear()
+            defaultDots()
+        }else{
+            Toast.makeText(requireContext(), "Incorrect password", Toast.LENGTH_SHORT).show()
+            enteredPassword.clear()
+            defaultDots()
+            deleteNumber()
+        }
+    }
+
+    private fun defaultDots(){
+        binding.dot1.setBackgroundResource(R.drawable.empty_dot_background)
+        binding.dot2.setBackgroundResource(R.drawable.empty_dot_background)
+        binding.dot3.setBackgroundResource(R.drawable.empty_dot_background)
+        binding.dot4.setBackgroundResource(R.drawable.empty_dot_background)
+    }
+
+    private fun updateDots() {
+        val dots = listOf(binding.dot1, binding.dot2, binding.dot3, binding.dot4)
+        for (i in dots.indices) {
+            if (i < enteredPassword.size) {
+                dots[i].setBackgroundResource(R.drawable.filled_dot_background)
+            } else {
+                dots[i].setBackgroundResource(R.drawable.empty_dot_background)
+            }
+        }
+    }
+
+    private fun deleteNumber(){
+        if(enteredPassword.isNotEmpty()){
+            enteredPassword.removeAt(enteredPassword.size-1)
+            updateDots()
+        }
+    }
+
+
 
 }
--- app/src/main/java/com/example/myapplication/NumpadAdapter.kt
+++ app/src/main/java/com/example/myapplication/NumpadAdapter.kt
@@ -5,7 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.RecyclerViewBinding
 
-class NumpadAdapter(private val buttons: List<NumpadClass>): RecyclerView.Adapter<NumpadAdapter.NumpadViewHolder>() {
+class NumpadAdapter(private val buttons: List<NumpadClass>,
+                    private val clickButton: (Int) -> Unit,
+                    private val deleteButtonClicked: () -> Unit):
+    RecyclerView.Adapter<NumpadAdapter.NumpadViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumpadViewHolder {
         return NumpadViewHolder(
@@ -29,7 +32,29 @@ class NumpadAdapter(private val buttons: List<NumpadClass>): RecyclerView.Adapte
         RecyclerView.ViewHolder(binding.root){
         fun onBind(num: NumpadClass){
             binding.keyBtn.apply {
-                text = num.toString() ?: ""
+                if (num.input in 0..9) {
+                    text = num.input.toString()
+                    setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+                }else if(num.input == R.drawable.fingerprint) {
+                    text = ""
+                    setCompoundDrawablesWithIntrinsicBounds(0, num.input, 0, 0)
+                    compoundDrawablePadding = 0
+                    setPadding(0, 40, 0, 0)
+                }else {
+                    text = ""
+                    setCompoundDrawablesWithIntrinsicBounds(0, num.input, 0, 0)
+                    compoundDrawablePadding = 0
+                    setPadding(0, 80, 0, 0)
+                }
+
+                setOnClickListener {
+                    if (num.input == R.drawable.delete_btn) {
+                        deleteButtonClicked()
+                    }else if(num.input == R.drawable.fingerprint){
+                    }else {
+                        clickButton(num.input)
+                    }
+                }
             }
         }
     }
